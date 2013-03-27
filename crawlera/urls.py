ae70826641f20226deeeedcce8c03bcd355a49bc
Sergey Sinkovskiy <glorybox.away@gmail.com>
2013-03-27 23:52:42 +0200

    added statis urlpatterns

--- crawlera/urls.py
+++ crawlera/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -35,4 +35,4 @@ urlpatterns = patterns('django.views.generic.simple',
     # url(r'^admin/', include(admin.site.urls)),
 )
 
-# urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
